a932f77d15e9a90f99a65fa3a27f1b542bb9d052
chaiyapluek <chaiyapluekmus@gmail.com>
2024-03-23 16:20:48 +0700

    fix: cart entity

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/Cart.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/Cart.java
@@ -1,22 +1,18 @@
 package com.kampus.kbazaar.cart;
 
 import com.kampus.kbazaar.product.Product;
-import lombok.Data;
-
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity(name = "cart")
 @Getter
 @Setter
+@AllArgsConstructor
 public class Cart {
-    @EmbeddedId private CartId cartId;
-    private Integer quantity;
-
-    public Cart(CartId cartId, Integer quantity) {
-        this.cartId = cartId;
-        this.quantity = quantity;
-    }
+    @Id private Long shopper_id;
+    private List<Product> products;
 }
--- kbazaar/src/main/java/com/kampus/kbazaar/cart/CartRepository.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart, CartId> {
-    
-}
+public interface CartRepository extends JpaRepository<Cart, Long> {}
