962ef8864aa92f3ad9547b73e66c8919e89785ba
pongsakon promma <pongsakonpromma@gmail.com>
2024-03-24 10:34:51 +0700

    feat : intial sql cart

--- kbazaar/src/main/resources/sql/schema/cart.sql
+++ kbazaar/src/main/resources/sql/schema/cart.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS cart (
-	shopper_id int4 NOT NULL,
-	products text NULL,
-	promotions text NULL,
-	CONSTRAINT cart_pk PRIMARY KEY (shopper_id)
+	cart_id varchar NOT NULL,
+	shopper_id int4 NULL,
+	promotions varchar NULL,
+	CONSTRAINT cart_pk PRIMARY KEY (cart_id)
 );
---
+++ kbazaar/src/main/resources/sql/schema/cart_item.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS cart_item (
+	cart_id varchar NOT NULL,
+	product_id varchar NOT NULL,
+	quantity int4 NULL
+);
